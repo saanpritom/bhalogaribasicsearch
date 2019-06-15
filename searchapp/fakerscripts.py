0e955d2baad7e01d04dc798bd55d7c08d0a9f79f
saanpritom <pritom.saan@gmail.com>
2019-06-15 23:41:08 +0600

    Problem with Fake data creation

--- searchapp/fakerscripts.py
+++ searchapp/fakerscripts.py
@@ -1,11 +1,41 @@
 from faker import Faker
 from faker.providers import automotive, barcode, company, currency, lorem
-#from searchapp.configs import *
-#from searchapp.models import CarModel, CarModelManager
-#from searchapp.serializers import CarSerializer
+from searchapp.serializers import CarSerializer
 
 
-fake = Faker()
+class FakerCarData:
+    fake = Faker()
+
+    def fake_headline(self):
+        return self.fake.name()
+
+    def fake_manufacturer(self):
+        return self.fake.company()
+
+    def fake_car_model(self):
+        return self.fake.company_suffix()
+
+    def fake_car_type(self):
+        return self.fake.cryptocurrency_name()
+
+    def fake_engine_type(self):
+        return self.fake.license_plate()
+
+    def fake_chasis_number(self):
+        return self.fake.ean(length=13)
+
+    def fake_description(self):
+        return self.fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None)
+
+    def fake_tags(self, **kwargs):
+        return str(kwargs['headline']) + ', ' + str(kwargs['manufacturer']) + ', ' + str(kwargs['car_model'] + ', ' +
+                    str(kwargs['car_type'] + ', ' + str(kwargs['engine_type'])))
+
+    def fake_price(self):
+        return self.fake.ean(length=8)
+
+
+"""fake = Faker()
 
 
 for _ in range(10):
@@ -19,4 +49,4 @@ for _ in range(10):
     print('tags -> ' + str(fake.company()) + ', ' + str(fake.company_suffix()) + ', ')
     print('price -> ' + str(fake.ean(length=8)) + ' ')
     print('is active -> ' + '1' + ' ')
-    print('created_at -> ' + '2019-06-18 12:00:05' + ' ')
+    print('created_at -> ' + '2019-06-18 12:00:05' + ' ')"""
--- searchapp/views.py
+++ searchapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.base import TemplateView
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from searchapp.configs import *
 from searchapp.serializers import CarSerializer
 from searchapp.models import CarModel, CarModelManager
 from searchapp.forms import CarCreationFrom
+from searchapp.fakerscripts import FakerCarData
 
 # Create your views here.
 class AllCarsAPIView(generics.ListAPIView):
@@ -15,9 +17,41 @@ class AllCarsAPIView(generics.ListAPIView):
 
 class CreateCarAPIView(generics.CreateAPIView):
     serializer_class = CarSerializer
+    model_class = CarModel
+    faker_script_class = FakerCarData()
 
-    def post(self, **kwargs):
-        return True
+    def post(self, request):
+
+        headline = self.faker_script_class.fake_headline()
+        manufacturer = self.faker_script_class.fake_manufacturer()
+        car_model = self.faker_script_class.fake_car_model()
+        car_type = self.faker_script_class.fake_car_type()
+        engine_type = self.faker_script_class.fake_engine_type()
+        chasis_number = self.faker_script_class.fake_chasis_number()
+        description = self.faker_script_class.fake_description()
+        price = self.faker_script_class.fake_price()
+
+        new_car_instance = self.model_class.objects.create_faker_data(headline = headline,
+                                           manufacturer = manufacturer,
+                                           car_model = car_model,
+                                           car_type = car_type,
+                                           engine_type = engine_type,
+                                           chasis_number = chasis_number,
+                                           description = description,
+                                           tags = self.faker_script_class.fake_tags(headline = headline,
+                                                                                    manufacturer = manufacturer,
+                                                                                    car_model = car_model,
+                                                                                    car_type = car_type,
+                                                                                    engine_type = engine_type),
+                                           price = price,
+                                           is_active = True)
+
+        if int(new_car_instance):
+            #return True
+            return Response(self.serializer_class.data, status=status.HTTP_201_CREATED)
+        else:
+            #return False
+            return Response(self.serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateFakeCarData(View):
@@ -40,7 +74,19 @@ class CreateFakeCarData(View):
             total_numbers = form.cleaned_data['total_numbers']
             form = self.form_class(initial=self.initial)
 
-            if self.api_view.post(total_numbers = total_numbers):
+            counter_flag = 1
+
+            for _ in range(int(total_numbers)):
+
+                hit_api = self.api_view.post()
+
+                if hit_api.status == status.HTTP_201_CREATED:
+                    counter_flag = counter_flag + 1
+                    continue
+                else:
+                    break
+
+            if counter_flag == int(total_numbers):
                 self.request_method = 'successpost'
             else:
                 self.request_method = 'errorpost'
