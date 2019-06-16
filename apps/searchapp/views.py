1a7e4929161547027457963db427c7d905bcc464
saanpritom <pritom.saan@gmail.com>
2019-06-16 22:02:18 +0600

    basic search api done

--- apps/searchapp/views.py
+++ apps/searchapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, Http404
 from django.views.generic import View
 from django.views.generic.base import TemplateView
+from django.db.models import Q
+from rest_framework.views import APIView
 from rest_framework import generics, status
 from rest_framework.response import Response
 from apps.searchapp.configs import *
@@ -32,4 +34,21 @@ class CarDetailAPIView(generics.RetrieveAPIView):
     def get(self, request, pk, format=None):
         car = self.get_object(pk)
         serializer = self.serializer_class(car)
+        return Response(serializer.data)
+
+
+class CarSearchAPIView(APIView):
+    model = CarModel
+    serializer_class = CarSerializer
+
+    def get_object(self, querystring):
+        try:
+            return self.model.objects.filter(Q(tags__icontains = querystring) | Q(description__icontains = querystring))
+        except self.model.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None, **kwargs):
+        keyword = kwargs['search']
+        cars = self.get_object(keyword)
+        serializer = self.serializer_class(cars, many=True)
         return Response(serializer.data)
