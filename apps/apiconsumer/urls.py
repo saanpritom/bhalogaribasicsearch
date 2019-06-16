f5d2fc5ec7a597d32bfb39c3692e858dd907b824
saanpritom <pritom.saan@gmail.com>
2019-06-16 21:32:36 +0600

    Navbar Integrated

--- apps/apiconsumer/urls.py
+++ apps/apiconsumer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apps.apiconsumer import views
 
 urlpatterns = [
-    path('', views.AllCarsView.as_view(), name='all-cars'),
+    path('', views.AllCarsView.as_view(), name='all-cars-view'),
     path('create/', views.CreateFakeCarData.as_view(), name='create-cars'),
     path('detail/<int:pk>/', views.DetailCarView.as_view(), name='detail-car'),
 ]
